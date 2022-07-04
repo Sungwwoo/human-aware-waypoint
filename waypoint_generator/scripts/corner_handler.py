ce05d31264eb91ea4a337f644cacb9e8ec4bd5f5
Sungwwoo <p11120076@gmail.com>
2022-07-04 18:48:20 +0900

    Add corner visualizer

--- waypoint_generator/scripts/corner_handler.py
+++ waypoint_generator/scripts/corner_handler.py
@@ -1,31 +1,74 @@
-from math import sqrt
+from math import sqrt, cos, sin
 import sys
+import yaml
 import heapq
 import rospy
-from nav_msgs.msg import OccupancyGrid
+import tf2_ros
+from tf.transformations import quaternion_from_euler
+from visualization_msgs.msg import Marker, MarkerArray
+from nav_msgs.msg import OccupancyGrid, Path
 from nav_msgs.srv import GetMap
+from geometry_msgs.msg import PoseStamped
+
 import numpy as np
 import cv2 as cv
 import cv_bridge
 import matplotlib.pyplot as plt
 
 
+def getTF(target_frame, source_frame):
+    # tf listner
+    tfBuffer = tf2_ros.Buffer()
+    tfListner = tf2_ros.TransformListener(tfBuffer)
+
+    while True:
+        try:
+            trans = tfBuffer.lookup_transform(target_frame, source_frame, rospy.Time())
+            return trans
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rospy.sleep(1)
+            continue
+
+
 class CornerHandler:
     def __init__(self):
 
         # Load params
+        self.global_plan_name = rospy.get_param("/global_plan_name", default="/move_base/NavfnROS/plan")
         self.map_file = rospy.get_param("/corner_detector/map_file", default="")
-        self.USE_APPROXIMATION = rospy.get_param("/corner_detect/use_approximation", default=True)
-        self.APPROX_PARAM = rospy.get_param("/corner_detect/approx_param", default=0.0005)
-        self.DISTANCE_THRESHOLD = rospy.get_param("/corner_detect/distance_threshold", default=5)
-        self.CONVEX_HULL_IDX = rospy.get_param("/corner_detect/convex_hull_idx", default=5)
+        self.show_image = rospy.get_param("/corner_detector/show_image", default=False)
+        self.USE_APPROXIMATION = rospy.get_param("/corner_detector/use_approximation", default=True)
+        self.APPROX_PARAM = rospy.get_param("/corner_detector/approx_param", default=0.005)
+        self.DISTANCE_THRESHOLD = rospy.get_param("/corner_detector/distance_threshold", default=5)
+        self.CONVEX_HULL_IDX = rospy.get_param("/corner_detector/convex_hull_idx", default=2)
 
+        print(self.show_image)
+        # Load map image
         if self.map_file == "":
-            rospy.logerr("No static map specified.")
+            rospy.logerr("No static map specified. Shutting down...")
             exit()
+        if self.map_file[-5:] == ".yaml":
+            self.image_dir = self.map_file[:-5] + ".pgm"
+        else:
+            rospy.logerr("Invalid map_file name. Map file must be end with '.yaml'")
+            exit()
+        self.img = cv.imread(self.image_dir, cv.IMREAD_GRAYSCALE)
+
+        # Load map data
+        with open(self.map_file) as file:
+            mapData = yaml.safe_load(file)
+
+        self.map_resolution = mapData["resolution"]
+        self.map_origin = mapData["origin"]
+
+        self.extracted = False
+        self.corners = []
+        self.g_path = []
+        self.global_goal = PoseStamped()
+        self.cornerMarkers = MarkerArray()
 
         # TODO
-        # Get static map data
+        # Get map data from map_server
         # rospy.wait_for_service("static_map")
         # try:
         #     get_map = rospy.ServiceProxy("static_map", GetMap)
@@ -33,19 +76,19 @@ class CornerHandler:
         # except rospy.ServiceException as e:
         #     rospy.logerr("Map service call failed: %s" % e)
 
+        self.pub_markers = rospy.Publisher("corner_markers", MarkerArray, queue_size=10)
         rospy.loginfo("Corner Detector Initialized")
 
     def extractCorners(self):
-        # Build CV image from occupancy grid map
-        img = cv.imread(self.map_file, cv.IMREAD_GRAYSCALE)
+        rospy.loginfo("Extracting corners")
 
         # Building Binary Image including Static Obstacle
-        ret, thresh = cv.threshold(img, 205, 255, cv.THRESH_BINARY_INV)
-        thresh = cv.bitwise_xor(img, thresh)
+        ret, thresh = cv.threshold(self.img, 205, 255, cv.THRESH_BINARY_INV)
+        thresh = cv.bitwise_xor(self.img, thresh)
         ret, thresh = cv.threshold(thresh, 205, 255, cv.THRESH_BINARY_INV)
 
-        # Converting to color image for colorized contour representation
-        img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
+        if self.show_image:
+            img = cv.cvtColor(self.img, cv.COLOR_GRAY2BGR)
 
         # Building Contour , Convex hull
         contours, hierarchy = cv.findContours(thresh, 1, 2)
@@ -56,9 +99,12 @@ class CornerHandler:
                 epsilon = self.APPROX_PARAM * cv.arcLength(cnts, True)
                 approx = cv.approxPolyDP(cnts, epsilon, True)
 
-                area.append(cv.moments(approx)["m00"])
+                area.append(cv.moments(cnts)["m00"])
 
                 # Extract contour points
+                if self.show_image:
+                    img = cv.drawContours(img, [approx], -1, color=(255, 0, 0))
+
                 for point in approx:
                     for data in point:
                         cntPoints.append(data)
@@ -68,7 +114,7 @@ class CornerHandler:
 
                 # Extract contour points
 
-                for point in approx:
+                for point in cnts:
                     for data in point:
                         cntPoints.append(data)
 
@@ -78,13 +124,16 @@ class CornerHandler:
         cnvxIdx = area.index(sortedArea[-self.CONVEX_HULL_IDX])
         hull = cv.convexHull(contours[cnvxIdx])
 
+        if self.show_image:
+            self.final_img = cv.drawContours(img, [hull], -1, color=(0, 0, 255))
+
         # Extract convex hull points
         for point in hull:
             for data in point:
                 cnvxPoints.append(data)
 
         # Remove contour points from convex hull points
-        self.corners = []
+
         for i in cntPoints:
             isValid = True
             for j in cnvxPoints:
@@ -92,16 +141,84 @@ class CornerHandler:
                     isValid = False
                     break
             if isValid:
+                i = i.astype(np.float32)
+                if self.show_image:
+                    img = cv.circle(img, (int(i[0]), int(i[1])), 4, (0, 0, 255), 1)
+                # Map the corner to the world coordinate
+                i[0] *= self.map_resolution  # resolution: meter/pixel
+                i[1] *= self.map_resolution
+
+                # Align to the origin
+                i[0] += self.map_origin[0]
+                i[1] += self.map_origin[1]
                 self.corners.append(i)
-        print(self.corners)
+        if self.corners == []:
+            self.extracted = False
+        else:
+            self.extracted = True
+
+        rospy.loginfo("Creating markers")
+        # Create Markers
+        orientation = quaternion_from_euler(0, 0, self.map_origin[2])
+        time = rospy.Time.now()
+
+        for i in range(0, len(self.corners)):
+            marker = Marker()
+            marker.header.frame_id = "map"
+            # marker.header.stamp = time
+            marker.id = i
+            marker.type = Marker.CYLINDER
+            marker.action = Marker.ADD
+            marker.scale.x = 0.5
+            marker.scale.y = 0.5
+            marker.scale.z = 0.1
+            marker.pose.position.x = self.corners[i][0]
+            marker.pose.position.y = self.corners[i][1]
+            marker.pose.position.z = 0.0
+            [marker.pose.orientation.x, marker.pose.orientation.y, marker.pose.orientation.z, marker.pose.orientation.w] = orientation
+            marker.color.r, marker.color.g, marker.color.b = 1, 1, 0
+            marker.color.a = 0.5
+            self.cornerMarkers.markers.append(marker)
+
+        return self.extracted
+
+    def pubMarkers(self):
+        self.pub_markers.publish(self.cornerMarkers)
 
     def createAvoidingArea(self):
+
         return
 
+    def getPathandGoal(self):
+        rospy.loginfo("Waiting for global plan to be published...")
+        g_path = rospy.wait_for_message(self.global_plan_name, Path)
+        for data in g_path.poses:
+            point = []
+            point.append(data.pose.position.x)
+            point.append(data.pose.position.y)
+            self.g_path.append(point)
+        return self.g_path
+
     def checkPath(self):
+
         return
 
     def avoidArea(self):
+
+        return
+
+    def init(self):
+        self.extractCorners()
+        self.createAvoidingArea()
+        self.getPathandGoal()
+
+    def run(self):
+        self.pub_markers.publish(self.cornerMarkers)
+
+        if self.show_image:
+            cv.namedWindow("Contour", cv.WINDOW_NORMAL)
+            cv.resizeWindow("Contour", width=1000, height=1000)
+            cv.imshow("Contour", self.final_img), cv.waitKey(1)
         return
 
 
@@ -110,4 +227,7 @@ if __name__ == "__main__":
 
     cornerHandler = CornerHandler()
 
-    cornerHandler.extractCorners()
+    cornerHandler.init()
+
+    while not rospy.is_shutdown():
+        cornerHandler.run()
